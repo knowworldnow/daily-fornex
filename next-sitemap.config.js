95e56fff092563373e6c82bfdd42a79bd47f8599
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-02 19:28:40 +0600

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
 
-const SITE_URL = process.env.NEXT_PUBLIC_URL
+const SITE_URL = process.env.NEXT_PUBLIC_URL;
 
 module.exports = {
-	siteUrl: SITE_URL,
-	generateRobotsTxt: true,
-	exclude: ['/wordpress-sitemap.xml'],
-	robotsTxtOptions: {
-		additionalSitemaps: [
-			`${SITE_URL}/wordpress-sitemap.xml`, // <==== Add here
-		],
-	},
-	transform: (config, path) => {
-		if (path.match(/\/\d{4}\/\d{2}\/\d{2}\/.*/gim)) {
-			return null
-		}
+  siteUrl: SITE_URL,
+  generateRobotsTxt: true,
+  exclude: ['/wordpress-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${SITE_URL}/wordpress-sitemap.xml`, // This adds the WordPress sitemap to the robots.txt
+    ],
+  },
+  transform: (config, path) => {
+    if (path.match(/\/\d{4}\/\d{2}\/\d{2}\/.*/gim)) {
+      return null; // Exclude date-based paths from the sitemap
+    }
 
-		return {
-			loc: path,
-			lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-		}
-	},
-}
+    return {
+      loc: path.endsWith('/') ? path : `${path}/`, // Ensure trailing slash
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
+};

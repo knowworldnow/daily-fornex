2813d83c93d4f5beae90e771dadeba84a61aee71
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-07 04:12:31 +0600

    Update SEO.tsx

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 interface Props {
   title?: string | null;
   description?: string | null;
   imageUrl?: string | null;
   url?: string | null;
+  siteName?: string | null;
 }
 
 /**
@@ -13,22 +15,23 @@ interface Props {
  * @param {Props} props The props object.
  * @param {string} props.title Used for the page title, og:title, twitter:title, etc.
  * @param {string} props.description Used for the meta description, og:description, twitter:description, etc.
- * @param {string} props.imageUrl Used for the og:image and twitter:image. NOTE: Must be an absolute url.
+ * @param {string} props.imageUrl Used for the og:image and twitter:image. NOTE: Must be an absolute URL.
  * @param {string} props.url Used for the og:url and twitter:url.
+ * @param {string} props.siteName Used for the og:site_name to define the site's name.
  *
  * @returns {React.ReactElement} The SEO component
  */
-export default function SEO({ title, description, imageUrl, url }: Props) {
-  if (!title && !description && !imageUrl && !url) {
-    return null;
-  }
-
+export default function SEO({ title, description, imageUrl, url, siteName }: Props) {
+  const router = useRouter();
+  const canonicalUrl = url || `https://dailyfornex.com${router.asPath}/`;
   const descriptionNoHtmlTags = description?.replace(/<[^>]*>?/gm, "") || "";
 
   return (
     <>
       <Head>
+        <link rel="canonical" href={canonicalUrl} />
         <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={siteName || "Daily Fornex"} />
         <meta property="twitter:card" content="summary_large_image" />
 
         {title && (
@@ -44,10 +47,7 @@ export default function SEO({ title, description, imageUrl, url }: Props) {
           <>
             <meta name="description" content={descriptionNoHtmlTags} />
             <meta property="og:description" content={descriptionNoHtmlTags} />
-            <meta
-              property="twitter:description"
-              content={descriptionNoHtmlTags}
-            />
+            <meta property="twitter:description" content={descriptionNoHtmlTags} />
           </>
         )}
 
@@ -58,12 +58,8 @@ export default function SEO({ title, description, imageUrl, url }: Props) {
           </>
         )}
 
-        {url && (
-          <>
-            <meta property="og:url" content={url} />
-            <meta property="twitter:url" content={url} />
-          </>
-        )}
+        <meta property="og:url" content={canonicalUrl} />
+        <meta property="twitter:url" content={canonicalUrl} />
       </Head>
     </>
   );

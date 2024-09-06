a6b502c7e7b96b6906f3e09e1b91c2287232bf3c
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-07 04:29:41 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { getWordPressProps, WordPressTemplate } from "@faustwp/core";
 import { WordPressTemplateProps } from "../types";
 import { GetStaticProps } from "next";
+import SEO from '../components/SEO/SEO'; // Import the SEO component
 
 export default function Page(props: WordPressTemplateProps) {
-  return <WordPressTemplate {...props} />;
+  return (
+    <>
+      <SEO 
+        title="Home - Daily Fornex" 
+        description="Welcome to Daily Fornex, your go-to resource for daily news, tips, and more."
+        url="https://dailyfornex.com/"
+      />
+      <WordPressTemplate {...props} />
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = (ctx) => {

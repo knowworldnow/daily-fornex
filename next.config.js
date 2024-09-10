86d6b1e2ebbe321af091653de0205ababb990ac3
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-11 02:57:43 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,102 +5,105 @@ const { createSecureHeaders } = require('next-secure-headers')
  * @type {import('next').NextConfig}
  **/
 module.exports = withFaust({
-	trailingSlash: true,
-	reactStrictMode: true,
-	experimental: {
-		typedRoutes: false,
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'http',
-				hostname: getWpHostname(),
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: getWpHostname(),
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: '0.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: '1.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: '2.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: '3.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'secure.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'images.pexels.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname: 'images.unsplash.com',
-				port: '',
-				pathname: '/**',
-			},
-			// from env
-			{
-				protocol: 'https',
-				hostname:
-					process.env.NEXT_PUBLIC_IMAGE_REMOTE_HOSTNAME_1 || '1.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-			{
-				protocol: 'https',
-				hostname:
-					process.env.NEXT_PUBLIC_IMAGE_REMOTE_HOSTNAME_2 || '1.gravatar.com',
-				port: '',
-				pathname: '/**',
-			},
-		],
-	},
-	async headers() {
-		return [
-			{
-				source: '/:path*',
-				headers: createSecureHeaders({
-					xssProtection: false,
-					frameGuard: [
-						'allow-from',
-						{ uri: process.env.NEXT_PUBLIC_WORDPRESS_URL },
-					],
-				}),
-			},
-		]
-	},
+  trailingSlash: true,
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: getWpHostname(),
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: getWpHostname(),
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '0.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '1.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '2.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '3.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      // from env
+      {
+        protocol: 'https',
+        hostname:
+          process.env.NEXT_PUBLIC_IMAGE_REMOTE_HOSTNAME_1 || '1.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname:
+          process.env.NEXT_PUBLIC_IMAGE_REMOTE_HOSTNAME_2 || '1.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: createSecureHeaders({
+          // XSS Protection enabled (consider setting it to true if not intentionally disabled)
+          xssProtection: true,
+          // Use frame-ancestors in CSP for framing instead of frameGuard
+          contentSecurityPolicy: {
+            directives: {
+              frameAncestors: [process.env.NEXT_PUBLIC_WORDPRESS_URL],
+            },
+          },
+        }),
+      },
+    ]
+  },
 })

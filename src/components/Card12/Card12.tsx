64e37dc74e993e53461d060a6447ea36e1d2f5d3
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-11 13:48:05 +0600

    Update Card12.tsx

--- src/components/Card12/Card12.tsx
+++ src/components/Card12/Card12.tsx
@@ -61,7 +61,11 @@ const Card12: FC<Card12Props> = ({ className = 'h-full', post }) => {
 					dangerouslySetInnerHTML={{ __html: excerpt || '' }}
 					className="mt-4 hidden text-neutral-500 sm:block dark:text-neutral-400"
 				></div>
-				<PostCardMeta className="mt-5 text-sm" meta={{ author, date }} />
+				<PostCardMeta
+					className="mt-5 text-sm"
+					meta={{ author, date }}
+					disableAuthorLink={true} // Disable the author link here
+				/>
 			</div>
 		</div>
 	)

22a5a3954c18882dc53ba3fb10e73c58c4281970
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-11 13:21:02 +0600

    Update Card10V2.tsx

--- src/components/Card10/Card10V2.tsx
+++ src/components/Card10/Card10V2.tsx
@@ -11,53 +11,53 @@ import { getPostDataFromPostFragment } from '@/utils/getPostDataFromPostFragment
 export interface Card10V2Props extends CommonPostCardProps {}
 
 const Card10V2: FC<Card10V2Props> = ({ className = 'h-full', post }) => {
-	const {
-		title,
-		link,
-		date,
-		categories,
-		excerpt,
-		author,
-		postFormats,
-		featuredImage,
-		ncPostMetaData,
-		commentCount,
-		uri,
-		databaseId,
-	} = getPostDataFromPostFragment(post)
+    const {
+        title,
+        link,
+        date,
+        categories,
+        excerpt,
+        author,
+        postFormats,
+        featuredImage,
+        ncPostMetaData,
+        commentCount,
+        uri,
+        databaseId,
+    } = getPostDataFromPostFragment(post)
 
-	const [isHover, setIsHover] = useState(false)
+    const [isHover, setIsHover] = useState(false)
 
-	return (
-		<div
-			className={`nc-Card10V2 relative flex flex-col ${className}`}
-			onMouseEnter={() => setIsHover(true)}
-			onMouseLeave={() => setIsHover(false)}
-		>
-			<div className="group aspect-h-12 aspect-w-16 relative z-0 block w-full flex-shrink-0 overflow-hidden rounded-3xl sm:aspect-h-9">
-				<div>
-					<PostFeaturedMedia post={post} isHover={isHover} />
-				</div>
+    return (
+        <div
+            className={`nc-Card10V2 relative flex flex-col ${className}`}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+        >
+            <div className="group aspect-h-12 aspect-w-16 relative z-0 block w-full flex-shrink-0 overflow-hidden rounded-3xl sm:aspect-h-9">
+                <div>
+                    <PostFeaturedMedia post={post} isHover={isHover} />
+                </div>
 
-				<Link
-					href={uri || ''}
-					className="absolute inset-0 bg-neutral-900 bg-opacity-20 opacity-0 transition-opacity"
-				></Link>
-			</div>
-			<div className="absolute inset-x-3 top-3 flex items-start justify-between space-x-4 rtl:space-x-reverse">
-				<CategoryBadgeList categories={categories?.nodes || []} />
-				<PostCardSaveAction
-					postDatabseId={databaseId}
-					readingTime={ncPostMetaData?.readingTime || 1}
-					hidenReadingTime
-				/>
-			</div>
+                <Link
+                    href={uri || ''}
+                    className="absolute inset-0 bg-neutral-900 bg-opacity-20 opacity-0 transition-opacity"
+                ></Link>
+            </div>
+            <div className="absolute inset-x-3 top-3 flex items-start justify-between space-x-4 rtl:space-x-reverse">
+                <CategoryBadgeList categories={categories?.nodes || []} />
+                <PostCardSaveAction
+                    postDatabseId={databaseId}
+                    readingTime={ncPostMetaData?.readingTime || 1}
+                    hidenReadingTime
+                />
+            </div>
 
-			<div className="mt-4 space-y-2.5 px-2 sm:px-4">
-				<PostCardMetaV2 meta={{ author, date, title, uri }} />
-			</div>
-		</div>
-	)
+            <div className="mt-4 space-y-2.5 px-2 sm:px-4">
+                <PostCardMetaV2 meta={{ author, date, title, uri }} disableAuthorLink={true} />
+            </div>
+        </div>
+    )
 }
 
 export default Card10V2

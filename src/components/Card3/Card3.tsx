b9fb445521e46d1089c3f9995a3a0671f16f1c3e
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-11 14:19:35 +0600

    Update Card3.tsx

--- src/components/Card3/Card3.tsx
+++ src/components/Card3/Card3.tsx
@@ -1,32 +1,19 @@
 import { FC } from 'react'
+import NcImage from '@/components/NcImage/NcImage'
+import PostCardMeta from '@/components/PostCardMeta/PostCardMeta'
 import PostCardSaveAction from '@/components/PostCardSaveAction/PostCardSaveAction'
 import PostCardLikeAndComment from '@/components/PostCardLikeAndComment/PostCardLikeAndComment'
 import CategoryBadgeList from '@/components/CategoryBadgeList/CategoryBadgeList'
 import PostTypeFeaturedIcon from '@/components/PostTypeFeaturedIcon/PostTypeFeaturedIcon'
-import PostFeaturedMedia from '@/components/PostFeaturedMedia/PostFeaturedMedia'
 import Link from 'next/link'
 import { CommonPostCardProps } from '../Card2/Card2'
 import { getPostDataFromPostFragment } from '@/utils/getPostDataFromPostFragment'
-import MyImage from '../MyImage'
 
-export interface Card19Props extends CommonPostCardProps {
-	ratio?: string
-	titleClass?: string
-	hoverClass?: string
-	showCategories?: boolean
-}
+export interface Card3Props extends CommonPostCardProps {}
 
-const Card19: FC<Card19Props> = ({
-	className = 'h-full',
-	titleClass = 'text-xl sm:text-2xl xl:text-3xl',
-	ratio = 'aspect-w-4 sm:aspect-w-3 aspect-h-3',
-	post,
-	hoverClass = '',
-	showCategories = true,
-}) => {
+const Card3: FC<Card3Props> = ({ className = 'h-full', post }) => {
 	const {
 		title,
-		link,
 		date,
 		categories,
 		excerpt,
@@ -39,75 +26,82 @@ const Card19: FC<Card19Props> = ({
 		databaseId,
 	} = getPostDataFromPostFragment(post)
 
-	const renderMeta = () => {
-		return (
-			<div className="inline-flex items-center text-xs text-neutral-300">
-				<h2
-					dangerouslySetInnerHTML={{ __html: title }}
-					className={`block font-semibold text-white ${titleClass}`}
-				></h2>
-			</div>
-		)
-	}
-
 	return (
 		<div
-			className={`nc-Card19 group relative flex flex-col overflow-hidden rounded-xl ${hoverClass} ${className}`}
-			data-nc-id="Card19"
+			className={`nc-Card3 group relative flex flex-row items-center ${className}`}
 		>
-			<div className="absolute inset-x-0 top-0 z-[-1] flex items-center justify-between p-3 opacity-0 transition-all duration-300 group-hover:z-10 group-hover:opacity-100">
-				<PostCardLikeAndComment
-					className="relative"
-					commentCount={commentCount || 0}
-					linkToPost={uri || ''}
-					likeCount={ncPostMetaData?.likesCount || 0}
-					postDatabseId={databaseId || 0}
-					viewCount={ncPostMetaData?.viewsCount || 0}
-				/>
-				<PostCardSaveAction
-					className="relative"
-					readingTime={ncPostMetaData?.readingTime || 1}
-					postDatabseId={databaseId || 0}
-				/>
-			</div>
-			<div className={`relative flex w-full items-start ${ratio}`}></div>
-			{postFormats === 'audio' ? (
-				<div className="absolute inset-0">
-					<PostFeaturedMedia post={post} />
+			<div className="flex flex-grow flex-col">
+				<div className="space-y-3.5">
+					<CategoryBadgeList categories={categories?.nodes || []} />
+					<Link href={uri || ''} className="block">
+						<h2
+							className={`nc-card-title block text-sm font-medium text-neutral-900 sm:text-base sm:font-semibold xl:text-lg dark:text-neutral-100`}
+						>
+							<span
+								dangerouslySetInnerHTML={{ __html: title }}
+								className="line-clamp-2"
+								title={title || ''}
+							></span>
+						</h2>
+						<div className="hidden sm:mt-2 sm:block">
+							<div
+								dangerouslySetInnerHTML={{ __html: excerpt || '' }}
+								className="line-clamp-2 text-sm text-neutral-500 dark:text-neutral-400"
+							></div>
+						</div>
+					</Link>
+
+					{/* Updated PostCardMeta component without author link */}
+					<PostCardMeta
+						meta={{
+							author: { ...author, uri: '' }, // Removing the link from the author
+							date,
+						}}
+					/>
 				</div>
-			) : (
-				<Link href={uri}>
-					<MyImage
-						sizes="(max-width: 600px) 480px, 800px"
-						className="h-full w-full rounded-xl object-cover"
+				<div className="mt-5 flex flex-wrap items-center justify-between">
+					<PostCardLikeAndComment
+						className="relative"
+						commentCount={commentCount || 0}
+						linkToPost={uri || ''}
+						likeCount={ncPostMetaData?.likesCount || 0}
+						postDatabseId={databaseId || 0}
+						viewCount={ncPostMetaData?.viewsCount || 0}
+					/>
+					<PostCardSaveAction
+						className="relative"
+						readingTime={ncPostMetaData?.readingTime || 1}
+						postDatabseId={databaseId || 0}
+					/>
+				</div>
+			</div>
+
+			<div
+				className={`z-0 mb-5 ms-3 block w-24 flex-shrink-0 overflow-hidden rounded-3xl sm:mb-0 sm:ms-6 sm:w-36 md:w-44 xl:w-56`}
+			>
+				<Link
+					href={uri || ''}
+					className="aspect-h-1 aspect-w-1 relative block h-0 w-full"
+				>
+					<NcImage
+						containerClassName="absolute inset-0"
 						src={featuredImage?.sourceUrl || ''}
-						alt="post"
 						fill
+						alt={title || ''}
+						className="h-full w-full rounded-3xl object-cover transition-transform duration-500 group-hover:scale-105"
 					/>
-					<PostTypeFeaturedIcon
-						className="absolute left-3 top-3 group-hover:hidden"
-						postType={postFormats}
-						wrapSize="w-7 h-7"
-						iconSize="w-4 h-4"
-					/>
-					<span className="absolute inset-0 bg-black bg-opacity-10 opacity-0 transition-opacity group-hover:opacity-100"></span>
+					<span>
+						<PostTypeFeaturedIcon
+							className="absolute bottom-2 left-2"
+							postType={postFormats}
+							wrapSize="w-8 h-8"
+							iconSize="w-4 h-4"
+						/>
+					</span>
 				</Link>
-			)}
-			<Link
-				href={uri}
-				className="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-black opacity-80"
-			></Link>
-			<div className="absolute inset-x-0 bottom-0 flex flex-grow flex-col p-5 sm:p-10">
-				<Link href={uri} className="absolute inset-0"></Link>
-				{showCategories && (
-					<div className="mb-3">
-						<CategoryBadgeList categories={categories?.nodes || []} />
-					</div>
-				)}
-				{renderMeta()}
 			</div>
 		</div>
 	)
 }
 
-export default Card19
+export default Card3

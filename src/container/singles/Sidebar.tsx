a76d43ddaa03c816866acaf9bc58245fae763965
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-13 23:03:07 +0600

    Update Sidebar.tsx

--- src/container/singles/Sidebar.tsx
+++ src/container/singles/Sidebar.tsx
@@ -1,25 +1,53 @@
-import { TCategoryCardFull } from '@/components/CardCategory1/CardCategory1'
-import WidgetAddSubscriberForm from '@/components/WidgetAddSubscriberForm/WidgetAddSubscriberForm'
-import WidgetCategories from '@/components/WidgetCategories/WidgetCategories'
-import WidgetSocialsFollow from '@/components/WidgetSocialsFollow/WidgetSocialsFollow'
-import React, { FC } from 'react'
+import { TCategoryCardFull } from '@/components/CardCategory1/CardCategory1';
+import WidgetAddSubscriberForm from '@/components/WidgetAddSubscriberForm/WidgetAddSubscriberForm';
+import WidgetCategories from '@/components/WidgetCategories/WidgetCategories';
+import WidgetSocialsFollow from '@/components/WidgetSocialsFollow/WidgetSocialsFollow';
+import React, { FC, useEffect } from 'react';
 
 export interface SidebarProps {
-	className?: string
-	categories: TCategoryCardFull[] | null
+  className?: string;
+  categories: TCategoryCardFull[] | null;
 }
 
 export const Sidebar: FC<SidebarProps> = ({
-	className = 'space-y-6 ',
-	categories,
+  className = 'space-y-6 ',
+  categories,
 }) => {
-	return (
-		<div className={`nc-SingleSidebar lg:sticky lg:top-24 ${className}`}>
-			<WidgetAddSubscriberForm />
 
-			<WidgetSocialsFollow />
+  useEffect(() => {
+    // Initialize Google AdSense script
+    if (window) {
+      const adsbygoogle = window.adsbygoogle || [];
+      adsbygoogle.push({});
+    }
+  }, []);
 
-			<WidgetCategories categories={categories || []} />
-		</div>
-	)
-}
+  return (
+    <div className={`nc-SingleSidebar lg:sticky lg:top-24 ${className}`}>
+      <WidgetAddSubscriberForm />
+
+      <WidgetSocialsFollow />
+
+      {/* Google AdSense Ad */}
+      <div className="adsense-widget sticky">
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7892867039237421"
+          crossOrigin="anonymous"
+        ></script>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-7892867039237421"
+          data-ad-slot="3608941742"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </div>
+
+      <WidgetCategories categories={categories || []} />
+    </div>
+  );
+};
+
+export default Sidebar;

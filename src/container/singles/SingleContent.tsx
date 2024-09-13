15f0fc8ab7eff2e32bf455f5d7ae147eb609ad7d
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-14 02:24:50 +0600

    Update SingleContent.tsx

--- src/container/singles/SingleContent.tsx
+++ src/container/singles/SingleContent.tsx
@@ -16,250 +16,250 @@ import TableContentAnchor from './TableContentAnchor'
 import Alert from '@/components/Alert'
 import { clsx } from 'clsx'
 import { useMusicPlayer } from '@/hooks/useMusicPlayer'
-import { insertAdsIntoContent } from '@/utils/insertAdsIntoContent';
-import React from 'react' // Make sure to import React
 
 export interface SingleContentProps {
-  post: GetPostSiglePageQuery['post']
+	post: GetPostSiglePageQuery['post']
 }
 
 const SingleContent: FC<SingleContentProps> = ({ post }) => {
-  const endedAnchorRef = useRef<HTMLDivElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
-  const progressRef = useRef<HTMLButtonElement>(null)
-  const [isShowScrollToTop, setIsShowScrollToTop] = useState<boolean>(false)
+	const endedAnchorRef = useRef<HTMLDivElement>(null)
+	const contentRef = useRef<HTMLDivElement>(null)
+	const progressRef = useRef<HTMLButtonElement>(null)
+	//
+	const [isShowScrollToTop, setIsShowScrollToTop] = useState<boolean>(false)
+	//
 
-  const endedAnchorEntry = useIntersectionObserver(endedAnchorRef, {
-    threshold: 0,
-    root: null,
-    rootMargin: '0%',
-    freezeOnceVisible: false,
-  })
+	const endedAnchorEntry = useIntersectionObserver(endedAnchorRef, {
+		threshold: 0,
+		root: null,
+		rootMargin: '0%',
+		freezeOnceVisible: false,
+	})
 
-  const {
-    content,
-    author,
-    databaseId,
-    commentCount,
-    commentStatus,
-    tags,
-    status,
-    date,
-  } = getPostDataFromPostFragment(post || {})
+	//
+	const {
+		content,
+		author,
+		databaseId,
+		commentCount,
+		commentStatus,
+		tags,
+		status,
+		date,
+	} = getPostDataFromPostFragment(post || {})
 
-  // Process the content to insert ads
-  const contentElements = insertAdsIntoContent(content || '')
+	//
 
-  useEffect(() => {
-    const handleProgressIndicator = () => {
-      const entryContent = contentRef.current
-      const progressBarContent = progressRef.current
+	useEffect(() => {
+		const handleProgressIndicator = () => {
+			const entryContent = contentRef.current
+			const progressBarContent = progressRef.current
 
-      if (!entryContent || !progressBarContent) {
-        return
-      }
+			if (!entryContent || !progressBarContent) {
+				return
+			}
 
-      const totalEntryH = entryContent.offsetTop + entryContent.offsetHeight
-      let winScroll =
-        document.body.scrollTop || document.documentElement.scrollTop
+			const totalEntryH = entryContent.offsetTop + entryContent.offsetHeight
+			let winScroll =
+				document.body.scrollTop || document.documentElement.scrollTop
 
-      let scrolled = totalEntryH ? (winScroll / totalEntryH) * 100 : 0
+			let scrolled = totalEntryH ? (winScroll / totalEntryH) * 100 : 0
 
-      progressBarContent.innerText = scrolled.toFixed(0) + '%'
+			progressBarContent.innerText = scrolled.toFixed(0) + '%'
 
-      if (scrolled >= 100) {
-        setIsShowScrollToTop(true)
-      } else {
-        setIsShowScrollToTop(false)
-      }
-    }
+			if (scrolled >= 100) {
+				setIsShowScrollToTop(true)
+			} else {
+				setIsShowScrollToTop(false)
+			}
+		}
 
-    const handleProgressIndicatorHeadeEvent = () => {
-      window?.requestAnimationFrame(handleProgressIndicator)
-    }
-    handleProgressIndicator()
-    window?.addEventListener('scroll', handleProgressIndicatorHeadeEvent)
-    return () => {
-      window?.removeEventListener('scroll', handleProgressIndicatorHeadeEvent)
-    }
-  }, [])
+		const handleProgressIndicatorHeadeEvent = () => {
+			window?.requestAnimationFrame(handleProgressIndicator)
+		}
+		handleProgressIndicator()
+		window?.addEventListener('scroll', handleProgressIndicatorHeadeEvent)
+		return () => {
+			window?.removeEventListener('scroll', handleProgressIndicatorHeadeEvent)
+		}
+	}, [])
 
-  const renderAlert = () => {
-    if (status === 'publish') {
-      return null
-    }
-    if (status === 'future') {
-      return (
-        <Alert type="warning">
-          This post is scheduled. It will be published on {date}.
-        </Alert>
-      )
-    }
-    return (
-      <Alert type="warning">
-        This post is {status}. It will not be visible on the website until it is
-        published.
-      </Alert>
-    )
-  }
+	const renderAlert = () => {
+		if (status === 'publish') {
+			return null
+		}
+		if (status === 'future') {
+			return (
+				<Alert type="warning">
+					This post is scheduled. It will be published on {date}.
+				</Alert>
+			)
+		}
+		return (
+			<>
+				<Alert type="warning">
+					This post is {status}. It will not be visible on the website until it
+					is published.
+				</Alert>
+			</>
+		)
+	}
 
-  const showLikeAndCommentSticky =
-    !endedAnchorEntry?.intersectionRatio &&
-    (endedAnchorEntry?.boundingClientRect.top || 0) > 0
+	const showLikeAndCommentSticky =
+		!endedAnchorEntry?.intersectionRatio &&
+		(endedAnchorEntry?.boundingClientRect.top || 0) > 0
 
-  return (
-    <div className="relative flex flex-col">
-      <div className="nc-SingleContent flex-1 space-y-10">
-        {/* Render alert if needed */}
-        {renderAlert()}
+	return (
+		<div className="relative flex flex-col">
+			<div className="nc-SingleContent flex-1 space-y-10">
+				{/*    */}
+				{renderAlert()}
 
-        {/* ENTRY CONTENT */}
-        <div
-          // Do not remove this id
-          id="single-entry-content"
-          className="prose mx-auto max-w-screen-md lg:prose-lg dark:prose-invert"
-          ref={contentRef}
-        >
-          {contentElements.map((element, index) => (
-            <React.Fragment key={index}>{element}</React.Fragment>
-          ))}
-        </div>
+				{/* ENTRY CONTENT */}
+				<div
+					// not remove this id
+					id="single-entry-content"
+					className="prose mx-auto max-w-screen-md lg:prose-lg dark:prose-invert"
+					ref={contentRef}
+					dangerouslySetInnerHTML={{ __html: content }}
+				/>
 
-        {/* TAGS */}
-        {tags?.nodes?.length ? (
-          <div className="mx-auto flex max-w-screen-md flex-wrap">
-            {tags.nodes.map((item) => (
-              <Tag
-                hideCount
-                key={item.databaseId}
-                name={'#' + (item.name || '')}
-                uri={item.uri || ''}
-                className="mb-2 me-2 border border-neutral-200 dark:border-neutral-800"
-              />
-            ))}
-          </div>
-        ) : null}
+				{/* TAGS */}
+				{tags?.nodes?.length ? (
+					<div className="mx-auto flex max-w-screen-md flex-wrap">
+						{tags.nodes.map(item => (
+							<Tag
+								hideCount
+								key={item.databaseId}
+								name={'#' + (item.name || '')}
+								uri={item.uri || ''}
+								className="mb-2 me-2 border border-neutral-200 dark:border-neutral-800"
+							/>
+						))}
+					</div>
+				) : null}
 
-        {/* AUTHOR */}
-        <div className="mx-auto max-w-screen-md border-b border-t border-neutral-100 dark:border-neutral-700"></div>
-        <div className="mx-auto max-w-screen-md">
-          <SingleAuthor author={author} />
-        </div>
+				{/* AUTHOR */}
+				<div className="mx-auto max-w-screen-md border-b border-t border-neutral-100 dark:border-neutral-700"></div>
+				<div className="mx-auto max-w-screen-md">
+					<SingleAuthor author={author} />
+				</div>
 
-        {/* COMMENTS LIST - Do not delete comments id */}
-        {commentStatus === 'open' ? (
-          <div
-            id="comments"
-            className="mx-auto max-w-screen-md scroll-mt-10 sm:scroll-mt-20"
-          >
-            <SingleCommentWrap
-              commentCount={commentCount || 0}
-              postDatabaseId={databaseId}
-            />
-          </div>
-        ) : null}
-        <div className="!my-0" ref={endedAnchorRef}></div>
-      </div>
+				{/* COMMENTS LIST - not delete comments id */}
+				{commentStatus === 'open' ? (
+					<div
+						id="comments"
+						className="mx-auto max-w-screen-md scroll-mt-10 sm:scroll-mt-20"
+					>
+						<SingleCommentWrap
+							commentCount={commentCount || 0}
+							postDatabaseId={databaseId}
+						/>
+					</div>
+				) : null}
+				<div className="!my-0" ref={endedAnchorRef}></div>
+			</div>
 
-      {/* Sticky action */}
-      <StickyAction
-        showLikeAndCommentSticky={showLikeAndCommentSticky}
-        isShowScrollToTop={isShowScrollToTop}
-        post={post}
-        ref={progressRef}
-      />
-    </div>
-  )
+			{/* sticky action */}
+			<StickyAction
+				showLikeAndCommentSticky={showLikeAndCommentSticky}
+				isShowScrollToTop={isShowScrollToTop}
+				post={post}
+				ref={progressRef}
+			/>
+		</div>
+	)
 }
 
 const StickyAction = forwardRef(function (
-  {
-    showLikeAndCommentSticky,
-    post,
-    isShowScrollToTop,
-  }: {
-    showLikeAndCommentSticky: boolean
-    post: GetPostSiglePageQuery['post']
-    isShowScrollToTop: boolean
-  },
-  progressRef,
+	{
+		showLikeAndCommentSticky,
+		post,
+		isShowScrollToTop,
+	}: {
+		showLikeAndCommentSticky: boolean
+		post: GetPostSiglePageQuery['post']
+		isShowScrollToTop: boolean
+	},
+	progressRef,
 ) {
-  const { content, databaseId, ncPostMetaData, uri, commentCount } =
-    getPostDataFromPostFragment(post || {})
+	//
+	const { content, databaseId, ncPostMetaData, uri, commentCount } =
+		getPostDataFromPostFragment(post || {})
 
-  const { postData: musicPlayerPostData } = useMusicPlayer()
+	const { postData: musicPlayerPostData } = useMusicPlayer()
 
-  const hasMusic = musicPlayerPostData?.databaseId
-  const stickyActionClassName = clsx(
-    'sticky z-40 mt-8 inline-flex self-center',
-    hasMusic ? 'bottom-14 sm:bottom-14' : 'bottom-5 sm:bottom-8',
-  )
+	const hasMusic = musicPlayerPostData?.databaseId
+	const stickyActionClassName = clsx(
+		'sticky z-40 mt-8 inline-flex self-center',
+		hasMusic ? 'bottom-14 sm:bottom-14' : 'bottom-5 sm:bottom-8',
+	)
 
-  return (
-    <div className={stickyActionClassName}>
-      <Transition
-        as={'div'}
-        show={showLikeAndCommentSticky}
-        enter="transition-opacity duration-75"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-150"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-        className={
-          'inline-flex items-center justify-center gap-1 self-center sm:gap-2'
-        }
-      >
-        <>
-          <div className="flex items-center justify-center gap-1 rounded-full bg-white p-1.5 text-xs shadow-lg ring-1 ring-neutral-900/5 ring-offset-1 sm:gap-2 dark:bg-neutral-800">
-            <PostCardLikeAction
-              likeCount={ncPostMetaData?.likesCount || 0}
-              postDatabseId={databaseId}
-            />
-            <div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
-            <PostCardCommentBtn
-              isATagOnSingle
-              commentCount={commentCount || 0}
-              linkToPost={uri || ''}
-            />
-            <div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
-            <NcBookmark postDatabseId={databaseId} />
-            <div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
+	return (
+		<div className={stickyActionClassName}>
+			<Transition
+				as={'div'}
+				show={showLikeAndCommentSticky}
+				enter="transition-opacity duration-75"
+				enterFrom="opacity-0"
+				enterTo="opacity-100"
+				leave="transition-opacity duration-150"
+				leaveFrom="opacity-100"
+				leaveTo="opacity-0"
+				className={
+					'inline-flex items-center justify-center gap-1 self-center sm:gap-2'
+				}
+			>
+				<>
+					<div className="flex items-center justify-center gap-1 rounded-full bg-white p-1.5 text-xs shadow-lg ring-1 ring-neutral-900/5 ring-offset-1 sm:gap-2 dark:bg-neutral-800">
+						<PostCardLikeAction
+							likeCount={ncPostMetaData?.likesCount || 0}
+							postDatabseId={databaseId}
+						/>
+						<div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
+						<PostCardCommentBtn
+							isATagOnSingle
+							commentCount={commentCount || 0}
+							linkToPost={uri || ''}
+						/>
+						<div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
+						<NcBookmark postDatabseId={databaseId} />
+						<div className="h-4 border-s border-neutral-200 dark:border-neutral-700"></div>
 
-            <button
-              className={`h-9 w-9 items-center justify-center rounded-full bg-neutral-50 hover:bg-neutral-100 dark:bg-neutral-800 ${
-                isShowScrollToTop ? 'flex' : 'hidden'
-              }`}
-              onClick={() => {
-                window.scrollTo({ top: 0, behavior: 'smooth' })
-              }}
-              title="Go to top"
-            >
-              <ArrowUpIcon className="h-4 w-4" />
-            </button>
+						<button
+							className={h-9 w-9 items-center justify-center rounded-full bg-neutral-50 hover:bg-neutral-100 dark:bg-neutral-800 ${
+								isShowScrollToTop ? 'flex' : 'hidden'
+							}}
+							onClick={() => {
+								window.scrollTo({ top: 0, behavior: 'smooth' })
+							}}
+							title="Go to top"
+						>
+							<ArrowUpIcon className="h-4 w-4" />
+						</button>
 
-            <button
-              ref={progressRef as any}
-              className={`h-9 w-9 items-center justify-center ${
-                isShowScrollToTop ? 'hidden' : 'flex'
-              }`}
-              title="Go to top"
-              onClick={() => {
-                window.scrollTo({ top: 0, behavior: 'smooth' })
-              }}
-            >
-              %
-            </button>
-          </div>
+						<button
+							ref={progressRef as any}
+							className={h-9 w-9 items-center justify-center ${
+								isShowScrollToTop ? 'hidden' : 'flex'
+							}}
+							title="Go to top"
+							onClick={() => {
+								window.scrollTo({ top: 0, behavior: 'smooth' })
+							}}
+						>
+							%
+						</button>
+					</div>
 
-          <TableContentAnchor
-            className="flex items-center justify-center gap-2 rounded-full bg-white p-1.5 text-xs shadow-lg ring-1 ring-neutral-900/5 ring-offset-1 dark:bg-neutral-800"
-            content={content}
-          />
-        </>
-      </Transition>
-    </div>
-  )
+					<TableContentAnchor
+						className="flex items-center justify-center gap-2 rounded-full bg-white p-1.5 text-xs shadow-lg ring-1 ring-neutral-900/5 ring-offset-1 dark:bg-neutral-800"
+						content={content}
+					/>
+				</>
+			</Transition>
+		</div>
+	)
 })
 
 export default SingleContent

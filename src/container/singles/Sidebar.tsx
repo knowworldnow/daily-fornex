7b96c15ef5290b31019800c13b83ebe251b3582a
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-15 02:53:30 +0600

    Update Sidebar.tsx

--- src/container/singles/Sidebar.tsx
+++ src/container/singles/Sidebar.tsx
@@ -1,57 +1,18 @@
-import { TCategoryCardFull } from '@/components/CardCategory1/CardCategory1';
-import WidgetAddSubscriberForm from '@/components/WidgetAddSubscriberForm/WidgetAddSubscriberForm';
-import WidgetCategories from '@/components/WidgetCategories/WidgetCategories';
-import WidgetSocialsFollow from '@/components/WidgetSocialsFollow/WidgetSocialsFollow';
-import React, { FC, useEffect } from 'react';
-
-declare global {
-  interface Window {
-    adsbygoogle: any[];
-  }
-}
+import React, { FC } from 'react';
+import TableContent from './TableContentAnchor';
 
 export interface SidebarProps {
   className?: string;
-  categories: TCategoryCardFull[] | null;
+  content: string;
 }
 
 export const Sidebar: FC<SidebarProps> = ({
   className = 'space-y-6 ',
-  categories,
+  content,
 }) => {
-
-  useEffect(() => {
-    // Initialize Google AdSense script
-    if (window) {
-      window.adsbygoogle = window.adsbygoogle || [];
-      window.adsbygoogle.push({});
-    }
-  }, []);
-
   return (
     <div className={`nc-SingleSidebar lg:sticky lg:top-24 ${className}`}>
-      <WidgetAddSubscriberForm />
-
-      <WidgetSocialsFollow />
-
-      {/* Google AdSense Ad */}
-      <div className="adsense-widget sticky">
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7892867039237421"
-          crossOrigin="anonymous"
-        ></script>
-        <ins
-          className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-7892867039237421"
-          data-ad-slot="3608941742"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-      </div>
-
-      <WidgetCategories categories={categories || []} />
+      <TableContent content={content} />
     </div>
   );
 };

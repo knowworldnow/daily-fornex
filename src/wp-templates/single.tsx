de8b43c341e3234d88c2a37f3a8e8a765f3253ee
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-16 00:31:31 +0600

    Update single.tsx

--- src/wp-templates/single.tsx
+++ src/wp-templates/single.tsx
@@ -1,9 +1,30 @@
 import { gql } from "../__generated__";
+import {
+  GetPostSiglePageQuery,
+  NcgeneralSettingsFieldsFragmentFragment,
+  NcmazFcUserReactionPostActionEnum,
+  NcmazFcUserReactionPostNumberUpdateEnum,
+} from "../__generated__/graphql";
 import { FaustTemplate } from "@faustwp/core";
 import SingleContent from "@/container/singles/SingleContent";
+import SingleType1 from "@/container/singles/single/single";
+import { getPostDataFromPostFragment } from "@/utils/getPostDataFromPostFragment";
+import { Sidebar } from "@/container/singles/Sidebar";
 import PageLayout from "@/container/PageLayout";
 import { FOOTER_LOCATION, PRIMARY_LOCATION } from "@/contains/menu";
-import { getPostDataFromPostFragment } from "@/utils/getPostDataFromPostFragment";
+import dynamic from "next/dynamic";  // <-- Added the missing import for dynamic
+import { useEffect, useState } from "react";
+import { useMutation } from "@apollo/client";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores/store";
+import { TPostCard } from "@/components/Card2/Card2";
+import { useRouter } from "next/router";
+import { TCategoryCardFull } from "@/components/CardCategory1/CardCategory1";
+import SocialsShare from "@/components/SocialsShare/SocialsShare";
+
+const DynamicSingleRelatedPosts = dynamic(
+  () => import("@/container/singles/SingleRelatedPosts")
+);
 
 const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
   if (props.loading) {
@@ -13,8 +34,9 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
   const _post = props.data?.post || {};
   const {
     title,
+    ncPostMetaData,
     featuredImage,
-    content,
+    content,  // <-- Make sure content is being extracted here
     databaseId,
     excerpt,
   } = getPostDataFromPostFragment(_post);
@@ -28,9 +50,22 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
       pageDescription={excerpt || ""}
       generalSettings={props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment}
     >
-      <div>
-        <SingleContent post={_post} />
-      </div>
+      {ncPostMetaData?.showRightSidebar ? (
+        <div className="relative">
+          <div className="container flex flex-col my-10 lg:flex-row">
+            <div className="w-full lg:w-3/5 xl:w-2/3 xl:pe-20">
+              <SingleContent post={_post} />
+            </div>
+            <div className="w-full mt-12 lg:mt-0 lg:w-2/5 lg:ps-10 xl:ps-0 xl:w-1/3">
+              <Sidebar content={content || ""} />  {/* Pass the content to the Sidebar */}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <SingleContent post={_post} />
+        </div>
+      )}
     </PageLayout>
   );
 };
@@ -48,11 +83,11 @@ Component.variables = ({ databaseId }, ctx) => {
 Component.query = gql(`
   query GetPostSiglePage($databaseId: ID!, $post_databaseId: Int,$asPreview: Boolean = false, $headerLocation: MenuLocationEnum!, $footerLocation: MenuLocationEnum!) {
     post(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-      ...NcmazFcPostFullFields
+    ...NcmazFcPostFullFields
     }
     posts(where: {isRelatedOfPostId:$post_databaseId}) {
       nodes {
-        ...PostCardFieldsNOTNcmazMEDIA
+      ...PostCardFieldsNOTNcmazMEDIA
       }
     }
     categories(first:10, where: { orderby: COUNT, order: DESC }) {

d39a479b25e819bcecaea8240a19197d27c45302
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-16 00:49:27 +0600

    Update single.tsx

--- src/wp-templates/single.tsx
+++ src/wp-templates/single.tsx
@@ -14,18 +14,12 @@ import PageLayout from "@/container/PageLayout";
 import { FOOTER_LOCATION, PRIMARY_LOCATION } from "@/contains/menu";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import { NC_MUTATION_UPDATE_USER_REACTION_POST_COUNT } from "@/fragments/mutations";
 import { useMutation } from "@apollo/client";
-import { IS_DEV } from "@/contains/site-settings";
 import { useSelector } from "react-redux";
 import { RootState } from "@/stores/store";
-import useGetPostsNcmazMetaByIds from "@/hooks/useGetPostsNcmazMetaByIds";
 import { TPostCard } from "@/components/Card2/Card2";
 import { useRouter } from "next/router";
 import { TCategoryCardFull } from "@/components/CardCategory1/CardCategory1";
-import SingleTypeAudio from "@/container/singles/single-audio/single-audio";
-import SingleTypeVideo from "@/container/singles/single-video/single-video";
-import SingleTypeGallery from "@/container/singles/single-gallery/single-gallery";
 import SocialsShare from "@/components/SocialsShare/SocialsShare";
 
 const DynamicSingleRelatedPosts = dynamic(
@@ -42,10 +36,12 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
     title,
     ncPostMetaData,
     featuredImage,
-    content,  // <-- Make sure content is being extracted here
+    content,
     databaseId,
     excerpt,
   } = getPostDataFromPostFragment(_post);
+
+  const router = useRouter();
 
   return (
     <PageLayout
@@ -60,16 +56,28 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
         <div className="relative">
           <div className="container flex flex-col my-10 lg:flex-row">
             <div className="w-full lg:w-3/5 xl:w-2/3 xl:pe-20">
+              <SingleType1 post={_post} />
               <SingleContent post={_post} />
+              <SocialsShare link={router.asPath} />
             </div>
             <div className="w-full mt-12 lg:mt-0 lg:w-2/5 lg:ps-10 xl:ps-0 xl:w-1/3">
-              <Sidebar content={content || ""} />  {/* Pass the content to the Sidebar */}
+              <Sidebar content={content || ""} />
             </div>
           </div>
+          <DynamicSingleRelatedPosts
+            posts={(props.data?.posts?.nodes as TPostCard[]) || []}
+            postDatabaseId={databaseId}
+          />
         </div>
       ) : (
         <div>
+          <SingleType1 post={_post} />
           <SingleContent post={_post} />
+          <SocialsShare link={router.asPath} />
+          <DynamicSingleRelatedPosts
+            posts={(props.data?.posts?.nodes as TPostCard[]) || []}
+            postDatabaseId={databaseId}
+          />
         </div>
       )}
     </PageLayout>
@@ -89,14 +97,14 @@ Component.variables = ({ databaseId }, ctx) => {
 Component.query = gql(`
   query GetPostSiglePage($databaseId: ID!, $post_databaseId: Int,$asPreview: Boolean = false, $headerLocation: MenuLocationEnum!, $footerLocation: MenuLocationEnum!) {
     post(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-    ...NcmazFcPostFullFields
+      ...NcmazFcPostFullFields
     }
     posts(where: {isRelatedOfPostId:$post_databaseId}) {
       nodes {
-      ...PostCardFieldsNOTNcmazMEDIA
+        ...PostCardFieldsNOTNcmazMEDIA
       }
     }
-    categories(first:10, where: { orderby: COUNT, order: DESC }) {
+    categories(first: 10, where: { orderby: COUNT, order: DESC }) {
       nodes {
         ...NcmazFcCategoryFullFieldsFragment
       }

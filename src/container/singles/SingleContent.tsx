33db3090dc93ae148a4f6f7331933adc629735e9
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-16 13:22:51 +0600

    Update SingleContent.tsx

--- src/container/singles/SingleContent.tsx
+++ src/container/singles/SingleContent.tsx
@@ -1,100 +1,93 @@
-'use client'
-
-import { FC, useEffect, useRef } from 'react'
+import { FC, useRef } from 'react'
 import Tag from '@/components/Tag/Tag'
 import SingleAuthor from './SingleAuthor'
+import PostCardLikeAction from '@/components/PostCardLikeAction/PostCardLikeAction'
+import PostCardCommentBtn from '@/components/PostCardCommentBtn/PostCardCommentBtn'
 import { GetPostSiglePageQuery } from '@/__generated__/graphql'
 import { getPostDataFromPostFragment } from '@/utils/getPostDataFromPostFragment'
+import NcBookmark from '@/components/NcBookmark/NcBookmark'
 import SingleCommentWrap from './SingleCommentWrap'
-import Alert from '@/components/Alert'
+import { Sidebar } from './Sidebar'
+import DynamicSingleRelatedPosts from './SingleRelatedPosts'
 
 export interface SingleContentProps {
-    post: GetPostSiglePageQuery['post']
+  post: GetPostSiglePageQuery['post']
+  relatedPosts: GetPostSiglePageQuery['posts']['nodes']
 }
 
-const SingleContent: FC<SingleContentProps> = ({ post }) => {
-    const contentRef = useRef<HTMLDivElement>(null)
+const SingleContent: FC<SingleContentProps> = ({ post, relatedPosts }) => {
+  const contentRef = useRef<HTMLDivElement>(null)
 
-    const {
-        content,
-        author,
-        databaseId,
-        commentCount,
-        commentStatus,
-        tags,
-        status,
-        date,
-    } = getPostDataFromPostFragment(post || {})
+  const {
+    content,
+    author,
+    databaseId,
+    commentCount,
+    commentStatus,
+    title,
+    excerpt,
+    featuredImage,
+    tags,
+    date,
+  } = getPostDataFromPostFragment(post || {})
 
-    const renderAlert = () => {
-        if (status === 'publish') {
-            return null
-        }
-        if (status === 'future') {
-            return (
-                <Alert type="warning">
-                    This post is scheduled. It will be published on {date}.
-                </Alert>
-            )
-        }
-        return (
-            <Alert type="warning">
-                This post is {status}. It will not be visible on the website until it
-                is published.
-            </Alert>
-        )
-    }
+  return (
+    <div className="relative flex flex-col lg:flex-row">
+      <div className="w-full lg:w-3/4 xl:w-2/3 xl:pe-20">
+        <h1 className="text-3xl font-bold mb-4">{title}</h1>
+        {featuredImage && (
+          <img
+            src={featuredImage.sourceUrl}
+            alt={featuredImage.altText || title}
+            className="w-full h-auto mb-6"
+          />
+        )}
+        <p className="text-lg mb-6">{excerpt}</p>
+        <SingleAuthor author={author} date={date} />
+        
+        <div
+          ref={contentRef}
+          className="prose mx-auto max-w-screen-md lg:prose-lg dark:prose-invert mt-10"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
 
-    return (
-        <div className="relative flex flex-col">
-            <div className="nc-SingleContent flex-1 space-y-10">
-                {/* Alert for post status */}
-                {renderAlert()}
+        {tags?.nodes?.length ? (
+          <div className="flex flex-wrap mt-10">
+            {tags.nodes.map(item => (
+              <Tag
+                key={item.databaseId}
+                name={item.name || ''}
+                href={item.uri || ''}
+                className="me-2 mb-2"
+              />
+            ))}
+          </div>
+        ) : null}
 
-                {/* ENTRY CONTENT */}
-                <div
-                    id="single-entry-content"
-                    className="prose mx-auto max-w-screen-md lg:prose-lg dark:prose-invert"
-                    ref={contentRef}
-                    dangerouslySetInnerHTML={{ __html: content }}
-                />
-
-                {/* TAGS */}
-                {tags?.nodes?.length ? (
-                    <div className="mx-auto flex max-w-screen-md flex-wrap">
-                        {tags.nodes.map(item => (
-                            <Tag
-                                hideCount
-                                key={item.databaseId}
-                                name={'#' + (item.name || '')}
-                                uri={item.uri || ''}
-                                className="mb-2 me-2 border border-neutral-200 dark:border-neutral-800"
-                            />
-                        ))}
-                    </div>
-                ) : null}
-
-                {/* AUTHOR */}
-                <div className="mx-auto max-w-screen-md border-b border-t border-neutral-100 dark:border-neutral-700"></div>
-                <div className="mx-auto max-w-screen-md">
-                    <SingleAuthor author={author} />
-                </div>
-
-                {/* COMMENTS LIST */}
-                {commentStatus === 'open' ? (
-                    <div
-                        id="comments"
-                        className="mx-auto max-w-screen-md scroll-mt-10 sm:scroll-mt-20"
-                    >
-                        <SingleCommentWrap
-                            commentCount={commentCount || 0}
-                            postDatabaseId={databaseId}
-                        />
-                    </div>
-                ) : null}
-            </div>
+        <div className="flex items-center justify-between mt-10">
+          <PostCardLikeAction postId={databaseId} />
+          <PostCardCommentBtn commentCount={commentCount || 0} postUri={post.uri || ''} />
+          <NcBookmark postId={databaseId} />
         </div>
-    )
+
+        {commentStatus === 'open' && (
+          <SingleCommentWrap
+            commentCount={commentCount || 0}
+            postDatabaseId={databaseId}
+          />
+        )}
+
+        <DynamicSingleRelatedPosts
+          posts={relatedPosts}
+          postDatabaseId={databaseId}
+        />
+      </div>
+      
+      <div className="w-full lg:w-1/4 xl:w-1/3 mt-10 lg:mt-0">
+        <Sidebar content={content} />
+      </div>
+    </div>
+  )
 }
 
 export default SingleContent

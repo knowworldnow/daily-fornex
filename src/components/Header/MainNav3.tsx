20f259e7af6383cbad3e145075bf12243707694f
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-17 03:51:58 +0600

    Update MainNav3.tsx

--- src/components/Header/MainNav3.tsx
+++ src/components/Header/MainNav3.tsx
@@ -3,7 +3,7 @@ import SwitchDarkMode from '../SwitchDarkMode/SwitchDarkMode';
 import Navigation from '../Navigation/Navigation';
 import { MainNav1Props } from './MainNav1';
 import dynamic from 'next/dynamic';
-import { HeaderSearchForm, SearchIconBtn } from './HeaderSearch';
+import { HeaderSearchForm } from './HeaderSearch';
 
 const DynamicMenuBar = dynamic(() => import('@/components/MenuBar/MenuBar'), {
 	ssr: false,
@@ -19,14 +19,12 @@ const MainNav3: FC<MainNav3Props> = ({ menuItems }) => {
 					<div className="flex flex-1 items-center lg:hidden">
 						<DynamicMenuBar menuItems={menuItems} />
 					</div>
-
 					<div className="hidden items-center gap-x-3 sm:gap-x-8 lg:flex">
 						<div className="hidden h-8 border-s border-neutral-200 md:block dark:border-neutral-700"></div>
 						<div className="hidden w-64 max-w-xs sm:block xl:w-80">
 							<HeaderSearchForm />
 						</div>
 					</div>
-
 					<div className="flex flex-1 justify-end">
 						<Navigation
 							maxItemsToShow={3}
@@ -36,7 +34,9 @@ const MainNav3: FC<MainNav3Props> = ({ menuItems }) => {
 						/>
 						<div className="mx-2 hidden h-8 self-center border-l border-neutral-200 md:block dark:border-neutral-700"></div>
 						<SwitchDarkMode className="hidden self-center md:flex" />
-						<SearchIconBtn className="lg:hidden" />
+						<div className="lg:hidden">
+							<HeaderSearchForm />
+						</div>
 					</div>
 				</div>
 			</div>

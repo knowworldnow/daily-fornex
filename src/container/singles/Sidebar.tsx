e0b4345cf99af3eaab45181d4d81a04d45598565
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-16 14:09:18 +0600

    Update Sidebar.tsx

--- src/container/singles/Sidebar.tsx
+++ src/container/singles/Sidebar.tsx
@@ -1,18 +1,59 @@
-import React, { FC } from 'react'
-import TableContent from './TableContent'
+import { TCategoryCardFull } from '@/components/CardCategory1/CardCategory1';
+import WidgetAddSubscriberForm from '@/components/WidgetAddSubscriberForm/WidgetAddSubscriberForm';
+import WidgetCategories from '@/components/WidgetCategories/WidgetCategories';
+import WidgetSocialsFollow from '@/components/WidgetSocialsFollow/WidgetSocialsFollow';
+import React, { FC, useEffect } from 'react';
+
+declare global {
+  interface Window {
+    adsbygoogle: any[];
+  }
+}
 
 export interface SidebarProps {
-  className?: string
-  content: string
+  className?: string;
+  categories: TCategoryCardFull[] | null;
 }
 
 export const Sidebar: FC<SidebarProps> = ({
-  className = '',
-  content,
+  className = 'space-y-6 ',
+  categories,
 }) => {
+
+  useEffect(() => {
+    // Initialize Google AdSense script
+    if (window) {
+      window.adsbygoogle = window.adsbygoogle || [];
+      window.adsbygoogle.push({});
+    }
+  }, []);
+
   return (
     <div className={`nc-SingleSidebar lg:sticky lg:top-24 ${className}`}>
-      <TableContent content={content} />
+      <WidgetAddSubscriberForm />
+
+      <WidgetSocialsFollow />
+
+      {/* Google AdSense Ad */}
+      <div className="adsense-widget sticky">
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7892867039237421"
+          crossOrigin="anonymous"
+        ></script>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-7892867039237421"
+          data-ad-slot="3608941742"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </div>
+
+      <WidgetCategories categories={categories || []} />
     </div>
-  )
-}
+  );
+};
+
+export default Sidebar;

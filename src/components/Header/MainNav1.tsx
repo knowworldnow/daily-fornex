3f3f88be56fcc79f45fe8a64860cd6deb800ecf3
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-17 02:18:54 +0600

    Update MainNav1.tsx

--- src/components/Header/MainNav1.tsx
+++ src/components/Header/MainNav1.tsx
@@ -1,33 +1,44 @@
-import { FC } from 'react'
-import Navigation from '@/components/Navigation/Navigation'
-import MenuBar from '@/components/MenuBar/MenuBar'
-import Logo from '@/components/Logo/Logo'
-import { SearchIconBtn } from './HeaderSearch'
-import ThemeToggle from './ThemeToggle'
+import { FC } from 'react';
+import Navigation from '@/components/Navigation/Navigation';
+import MenuBar from '@/components/MenuBar/MenuBar';
+import { NC_PRIMARY_MENU_QUERY_FRAGMENT } from '@/fragments/menu';
+import { FragmentType } from '@/__generated__';
+import AvatarDropdown from './AvatarDropdown';
+import { SearchIconBtn } from './HeaderSearch';
 
 export interface MainNav1Props {
-  menuItems: any[]  // Replace 'any' with your actual menu item type
+	menuItems: FragmentType<typeof NC_PRIMARY_MENU_QUERY_FRAGMENT>[];
+	title?: string | null;
+	description?: string | null;
 }
 
 const MainNav1: FC<MainNav1Props> = ({ menuItems }) => {
-  return (
-    <header className="border-b border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-900">
-      <div className="container mx-auto px-4">
-        <div className="flex h-16 items-center justify-between">
-          <div className="flex items-center">
-            <MenuBar menuItems={menuItems} className="lg:hidden" />
-            <SearchIconBtn className="lg:hidden" />
-          </div>
-          <Navigation menuItems={menuItems} className="hidden lg:flex" />
-          <Logo /> {/* This will now switch between light and dark mode logos */}
-          <div className="flex items-center">
-            <ThemeToggle />
-            <SearchIconBtn className="hidden lg:flex" />
-          </div>
-        </div>
-      </div>
-    </header>
-  )
-}
+	return (
+		<div className="nc-MainNav1 relative z-10 border-b border-neutral-200/70 bg-white dark:border-transparent dark:bg-neutral-900">
+			<div className="container">
+				<div className="flex h-16 items-center justify-between py-3 sm:h-20 sm:py-4">
+					<div className="flex flex-1 items-center lg:hidden">
+						<MenuBar menuItems={menuItems} />
+					</div>
 
-export default MainNav1
+					<div className="flex flex-1 items-center justify-center space-x-4 sm:space-x-10 lg:justify-start 2xl:space-x-14 rtl:space-x-reverse">
+						<Navigation menuItems={menuItems} className="hidden lg:flex" />
+					</div>
+
+					<div className="flex flex-1 items-center justify-end space-x-1 text-neutral-700 rtl:space-x-reverse dark:text-neutral-100">
+						<div className="hidden items-center lg:flex">
+							<SearchIconBtn className="flex" />
+							<AvatarDropdown />
+						</div>
+						<div className="flex items-center lg:hidden">
+							<SearchIconBtn className="flex" />
+							<AvatarDropdown />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MainNav1;

801b87562337694d7de601885c892747a0e6bbb1
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-22 01:48:51 +0600

    Update page.tsx

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { getLatestPosts } from '../../lib/faust-api';
 import { getAllCategories } from '../../lib/faust-api';
-import { Post as PostType, Category, Comment, CommentAuthor } from '../../types';
+import { Category } from '../../types';
 
 export const revalidate = 300;
 
 export async function generateStaticParams() {
-  const posts = await getLatestPosts({ first: 20 }); // Adjust the number as needed
+  const posts = await getLatestPosts({ first: 20 });
   return posts.map((post) => ({
     slug: post.slug,
   }));
@@ -25,7 +25,6 @@ interface CategoryWithImage extends Category {
 export default async function CategoriesPage() {
   const categories: CategoryWithImage[] = await getAllCategories();
   
-  // Sort categories by post count in descending order
   categories.sort((a, b) => b.count - a.count);
 
   return (

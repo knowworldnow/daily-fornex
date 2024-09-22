70a174d5f46318adea05888cc65200e96dfa9f94
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-23 02:19:25 +0600

    Update SocialSharePanel.tsx

--- src/components/SocialSharePanel.tsx
+++ src/components/SocialSharePanel.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 interface SocialSharePanelProps {
   url: string;
@@ -29,32 +30,27 @@ const SocialSharePanel: React.FC<SocialSharePanelProps> = ({ url, title, descrip
   const shareLinks = [
     {
       name: 'Facebook',
-      icon: 'M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z',
-      color: '#1877F2',
+      icon: '/facebook.svg',
       url: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`
     },
     {
       name: 'Twitter',
-      icon: 'M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z',
-      color: '#1DA1F2',
+      icon: '/twitter.svg',
       url: `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(title)}`
     },
     {
       name: 'LinkedIn',
-      icon: 'M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z M2 4a2 2 0 114 0 2 2 0 01-4 0z',
-      color: '#0A66C2',
+      icon: '/linkedin.svg',
       url: `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}&summary=${encodeURIComponent(description)}`
     },
     {
       name: 'WhatsApp',
-      icon: 'M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.602.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347z',
-      color: '#25D366',
+      icon: '/whatsapp.svg',
       url: `https://wa.me/?text=${encodeURIComponent(title + ' ' + url)}`
     },
     {
       name: 'Pinterest',
-      icon: 'M12 0a12 12 0 00-4.37 23.17c-.1-.94-.2-2.43.04-3.47.22-.97 1.41-6.15 1.41-6.15s-.36-.72-.36-1.79c0-1.68.97-2.93 2.17-2.93 1.03 0 1.52.77 1.52 1.69 0 1.03-.66 2.57-1 4-.28 1.2.6 2.17 1.78 2.17 2.13 0 3.77-2.25 3.77-5.5 0-2.87-2.06-4.88-5.01-4.88-3.41 0-5.42 2.56-5.42 5.21 0 1.03.4 2.14.89 2.74a.36.36 0 01.08.35l-.33 1.36c-.05.22-.17.27-.4.16-1.5-.7-2.44-2.89-2.44-4.65 0-3.79 2.75-7.26 7.93-7.26 4.17 0 7.4 2.97 7.4 6.93 0 4.14-2.61 7.46-6.23 7.46-1.22 0-2.36-.63-2.75-1.38l-.75 2.85c-.27 1.04-1 2.35-1.49 3.15A12 12 0 0012 24a12 12 0 000-24z',
-      color: '#BD081C',
+      icon: '/pinterest.svg',
       url: `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(url)}&media=${encodeURIComponent(imageUrl)}&description=${encodeURIComponent(title)}`
     },
   ];
@@ -74,10 +70,13 @@ const SocialSharePanel: React.FC<SocialSharePanelProps> = ({ url, title, descrip
           className="block mb-2 last:mb-0 hover:opacity-80 transition-opacity"
           aria-label={`Share on ${link.name}`}
         >
-          <svg className="w-8 h-8" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <rect width="24" height="24" rx="4" fill={link.color} />
-            <path d={link.icon} fill="white" />
-          </svg>
+          <Image 
+            src={link.icon} 
+            alt={`${link.name} icon`} 
+            width={32} 
+            height={32} 
+            className="w-8 h-8" 
+          />
         </a>
       ))}
     </div>

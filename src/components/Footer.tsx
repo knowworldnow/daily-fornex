f6ad6dd4887ef97a8ef9f1ab5e209fca75ac701a
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-23 01:46:34 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const socialLinks = [
-  { name: 'Facebook', icon: 'M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z', color: '#1877F2', url: 'https://fb.com/dailyfornex' },
-  { name: 'Twitter', icon: 'M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z', color: '#1DA1F2', url: 'https://twitter.com/dailyfornex' },
-  { name: 'Instagram', icon: 'M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01M7.8 2h8.4C19.6 2 22 4.4 22 7.8v8.4a5.8 5.8 0 01-5.8 5.8H7.8C4.4 22 2 19.6 2 16.2V7.8A5.8 5.8 0 017.8 2m-.2 2A3.6 3.6 0 004 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 003.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 011.25 1.25A1.25 1.25 0 0117.25 8 1.25 1.25 0 0116 6.75a1.25 1.25 0 011.25-1.25M12 7a5 5 0 015 5 5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5z', color: '#E4405F', url: 'https://instagram.com/dailyfornex' },
-  { name: 'Pinterest', icon: 'M12 0a12 12 0 00-4.37 23.17c-.1-.94-.2-2.43.04-3.47.22-.97 1.41-6.15 1.41-6.15s-.36-.72-.36-1.79c0-1.68.97-2.93 2.17-2.93 1.03 0 1.52.77 1.52 1.69 0 1.03-.66 2.57-1 4-.28 1.2.6 2.17 1.78 2.17 2.13 0 3.77-2.25 3.77-5.5 0-2.87-2.06-4.88-5.01-4.88-3.41 0-5.42 2.56-5.42 5.21 0 1.03.4 2.14.89 2.74a.36.36 0 01.08.35l-.33 1.36c-.05.22-.17.27-.4.16-1.5-.7-2.44-2.89-2.44-4.65 0-3.79 2.75-7.26 7.93-7.26 4.17 0 7.4 2.97 7.4 6.93 0 4.14-2.61 7.46-6.23 7.46-1.22 0-2.36-.63-2.75-1.38l-.75 2.85c-.27 1.04-1 2.35-1.49 3.15A12 12 0 0012 24a12 12 0 000-24z', color: '#BD081C', url: 'https://pinterest.com/dailyfornex' },
-  { name: 'YouTube', icon: 'M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z', color: '#FF0000', url: 'https://youtube.com/dailyfornex' },
+  { name: 'Facebook', icon: '/facebook.svg', url: 'https://fb.com/dailyfornex' },
+  { name: 'Twitter', icon: '/twitter.svg', url: 'https://twitter.com/dailyfornex' },
+  { name: 'Instagram', icon: '/instagram.svg', url: 'https://instagram.com/dailyfornex' },
+  { name: 'Pinterest', icon: '/pinterest.svg', url: 'https://pinterest.com/dailyfornex' },
+  { name: 'YouTube', icon: '/youtube.svg', url: 'https://youtube.com/dailyfornex' },
 ];
 
 export default function Footer() {
@@ -42,10 +42,13 @@ export default function Footer() {
           <div className="flex space-x-4 mt-4 md:mt-0">
             {socialLinks.map((social) => (
               <a key={social.name} href={social.url} target="_blank" rel="noopener noreferrer" className="hover:opacity-80">
-                <svg className="w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <rect width="24" height="24" rx="4" fill={social.color} />
-                  <path d={social.icon} fill="white" />
-                </svg>
+                <Image 
+                  src={social.icon} 
+                  alt={`${social.name} icon`} 
+                  width={24} 
+                  height={24} 
+                  className="w-6 h-6 dark:invert" 
+                />
               </a>
             ))}
           </div>

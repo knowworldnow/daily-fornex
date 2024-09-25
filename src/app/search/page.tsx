350de4ac2d96703ff015893a323291c450d76700
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-26 01:56:17 +0600

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,7 +4,6 @@ import SearchResults from './SearchResults';
 export default function SearchPage() {
   return (
     <div>
-      <h1>Search Results</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <SearchResults />
       </Suspense>
